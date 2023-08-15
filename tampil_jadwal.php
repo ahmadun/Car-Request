88aeefc5f99d345e41dd7eb1207b8cea3b87a083
aini <122358601+Ainizoro@users.noreply.github.com>
2023-08-15 09:05:12 +0700

    Update tampil_jadwal.php

--- tampil_jadwal.php
+++ tampil_jadwal.php
@@ -1,28 +1,16 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title>Jadwal</title>
-</head>
 <!DOCTYPE html>
     <html lang="en">
       <head >
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Admin</title>
+        <title>USER</title>
         <!-- bootstrap 5 css -->
-        <link
-          rel="stylesheet"
-          href="http://192.168.29.55:8300/bootstrap.min.css"
-          integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK"
-          crossorigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="http://192.168.29.55:8300/bootstrap-icons.css"
-        />
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <style>
     li {
         list-style: none;
@@ -31,7 +19,7 @@ session_start();
     }
     a {
         text-decoration: none;
-        background: #87CEFA;
+       
     }
 
     .sidebar {
@@ -41,6 +29,7 @@ session_start();
         margin-left: -300px;
         transition: 0.4s;
         background: #87CEFA;
+        font-size: 15px;
     }
 
     .active-main-content {
@@ -60,63 +49,47 @@ session_start();
 </style>
       </head>
 
-      <body background="#87CEFA";>
+      <body>
         <div>
-          <div class="sidebar p-4 background #87CEFA" id="sidebar" >
-            <h4 class="mb-5 text-white">CAR REQUEST</h4>
-            <li>
-              <a class="text-white" href="halaman_admin.php">
-                <i class="bi bi-house mr-2"></i>
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a class="text-white" href="data_admin.php">
-                <i class='bi bi-truck'></i>
-               Input Request Car
-              </a>
-            </li>
-            <li>
-              <a class="text-white" href="laporan_admin.php">
-                <i class="bi bi-clipboard-data-fill"></i>
-                Laporan Car Request
-                <?php include('notification.php') ?>
-              </a>
-            </li>
-            <li>
-              <a class="text-white" href="add.php">
-                <i class='bi bi-person-plus'></i>
-                Add USER
-              </a>
-            </li>
+          <div class="sidebar p-4" id="sidebar" >
+            <h4 class="mb-5 text-black" style="font-size: 25px;">CAR REQUEST</h4>
              <li>
-              <a class="text-white" href="update_jadwal.php">
-                <i class='bi bi-arrow-repeat'></i>
-                Update Jadwal Car
-              </a>
-            </li>
-            <li>
-              <a class="text-white" href="index.php">
-                <i class='bi bi-box-arrow-right'></i>
-                Logout
-              </a>
-            </li>
+        <a class="text-white" href="halaman_GA.php">
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a class="text-white" href="request_ga.php">
+          Data Car Request
+          <?php include('notifikasi.php') ?>
+        </a>
+      </li>
+      <li>
+        <a class="text-white" href="laporan_ga.php">
+          Laporan Car Request
+        </a>
+      </li>
+      <li>
+        <a class="text-white" href="update_jadwal.php">
+          Update Jadwal Car
+        </a>
+      </li>
+      <a class="text-white" href="index.php">
+        Logout
+      </a>
+      </li>
         </div>
         </div>
         <section class="p-4" id="main-content">
-          <button class="btn btn-primary" id="button-toggle">
-            <i class="bi bi-list"></i>
+          <button class="btn btn-info" id="button-toggle">
+             <img src="list.png" height="30px" width="30px">
           </button>
           <div class="card mt-5">
             <div class="card-body">
               <h4>JADWAL KEBERANGKATAN BUS PLAN</h4>
               <p>
-
-
 <?php 
-session_start();
 echo @$_SESSION['pesan'];
-session_destroy();
 ?>
 <table border="1">
 	<tr>
