c2ba5b583e800c47379a54b5d384a9a798b706b0
aini <122358601+Ainizoro@users.noreply.github.com>
2023-08-15 09:08:18 +0700

    Update request_manager.php

--- request_manager.php
+++ request_manager.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <?php
 include('koneksi.php');
-$cekJumlah = mysqli_query($koneksi, "select count(id) as jumlah from data where notif_manager=0");
+$cekJumlah = mysqli_query($koneksi, "select count(id_data) as jumlah from data where notif_manager=0");
 $data = mysqli_fetch_assoc($cekJumlah);
 if ($data['jumlah'] > 0) {
   $query = "UPDATE data SET notif_manager=1 WHERE notif_manager=0";
@@ -14,64 +15,69 @@ if ($data['jumlah'] > 0) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>USER</title>
-  <!-- bootstrap 5 css -->
-  <link rel=”stylesheet” href=”css/bootstrap.min.css” />
-  <link rel="stylesheet" href="/assets/lib/font-awesome/css/font-awesome.css" type="text/css">
-  <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
-  <style>
+       <!-- bootstrap 5 css -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <style>
     li {
-      list-style: none;
-      margin: 20px 0 20px 0;
+        list-style: none;
+        margin: 20px 0 20px 0;
     }
+
     a {
-      text-decoration: none;
-      background: #87CEFA;
+        text-decoration: none;
+        background: #87CEFA;
     }
 
     .sidebar {
-      width: 250px;
-      height: 100vh;
-      position: fixed;
-      margin-left: -300px;
-      transition: 0.4s;
-      background: #87CEFA;
+        width: 250px;
+        height: 100vh;
+        position: fixed;
+        margin-left: -300px;
+        transition: 0.4s;
+        background: #87CEFA;
+        font-size: 15px;
     }
 
     .active-main-content {
-      margin-left: 250px;
-
+        margin-left: 250px;
+       
     }
 
     .active-sidebar {
-      margin-left: 0;
-      background: #87CEFA;
+        margin-left: 0;
+        background: #87CEFA;
     }
 
     #main-content {
-      transition: 0.4s;
+        transition: 0.4s;
+        
     }
-  </style>
-</head>
-<body background="#87CEFA" ;>
+</style>
+      </head>
+
+<body>
   <div>
-    <div class="sidebar p-4 background #87CEFA" id="sidebar">
-      <h4 class="mb-5 text-white">CAR REQUEST</h4>
+    <div class="sidebar p-4" id="sidebar">
+      <h4 class="mb-5 text-black" style="font-size: 25px;">CAR REQUEST</h4>
       <li>
         <a class="text-white" href="halaman_manager.php">
           Dashboard
         </a>
       </li>
       <li>
-        <a class="text-white" href="request_manager.php">
-          Data Car Request
-          <?php include('notification.php') ?>
+        <a class="text-white" href="pengajuan_manager.php">
+          Input Request Car
         </a>
       </li>
-      <li>
-        <a class="text-white" href="data_manager.php">
-          Input Request Car
+       <li>
+        <a class="text-white" href="request_manager.php">
+          Data Car Request
+          <?php include('notifikasi.php') ?>
         </a>
       </li>
       <li>
@@ -79,63 +85,75 @@ if ($data['jumlah'] > 0) {
           Laporan Car Request
          </li>
       <a class="text-white" href="index.php">
-         Logout
+       Logout
       </a>
       </li>
   </div>
   </div>
   <section class="p-4" id="main-content">
-    <button class="btn btn-primary" id="button-toggle">
-      <img src="list.png" height="30px" width="30px">
-    </button>
-    <div style="border:1px solid rgb(238,238,238); padding:auto; overflow:auto; width:auto; height:auto;">
+          <button class="btn btn-info" id="button-toggle">
+            <img src="list.png" height="30px" width="30px">
+          </button>
+      <div style="border:1px; padding:10px; overflow:50px; width:50%; height:auto;">
+   
       <?php
       include "koneksi.php";
       $query = mysqli_query($koneksi, "SELECT * FROM data");
       ?>
       <form action="proses_manager.php" method="post">
-        <table width="90%" border="0" align="center" cellpadding="0" cellspacing="0">
+        <table width="150%" border="0" align="center" cellpadding="0" cellspacing="0">
           <tr bgcolor="white"></br>
-            <h4>Laporan Car Request</h4>
+            <h4>Pengajuan Car Request</h4><br>
           <tr>
-            <th width="70">No</td>
-            <th width="70">NIK</td>
-            <th width="70">Nama</td>
-            <th width="70">Section</td>
-            <th width="70">Tujuan</td>
-            <th width="70">Alasan</td>
-            <th width="150">Keberangkatan</td>
-            <th width="150">Status Supervisor</td>
-            <th width="150">Status Manager</td>
+            <th width="20">No</td>
+
+            <th width="30">NIK</td>
+            <th width="30">Nama</td>
+            <th width="30">Section</td>
+            <th width="30">Posisi</td>
+            <th width="30">Tujuan</td>
+            <th width="50">Alasan</td>
+            <th width="50">Keberangkatan</td>
+            <th width="10">Status Manager</td>
+
           </tr>
-          <?php if (mysqli_num_rows($query) > 0) { ?>
-            <?php
-            $no = 1;
-            while ($data = mysqli_fetch_array($query)) {
-            ?>
-              <tr>
+          <?php
+        $no = 1;
+        while ($data = mysqli_fetch_array($query)) {
+        ?>
+            <tr>
                 <td><?php echo $no ?></td>
                 <td><?php echo $data["nik"]; ?></td>
                 <td><?php echo $data["nama"]; ?></td>
                 <td><?php echo $data["section"]; ?></td>
+                <td><?php echo $data["posisi"]; ?></td>
                 <td><?php echo $data["tujuan"]; ?></td>
                 <td><?php echo $data["alasan"]; ?></td>
                 <td><?php echo $data["keberangkatan"]; ?></td>
-                <td><?php echo $data["status_spv"]; ?></td>
-                <td>
+               <?php if ($data["posisi"] !== "manager") : ?>
+                    <td>
                   <?php if ($data["status_manager"] == NULL) : ?>
-                    <input type="radio" name="status_manager[<?= $data['id'] ?>]" value="diterima" required>Diterima
-                    <input type="radio" name="status_manager[<?= $data['id'] ?>]" value="ditolak" required>Ditolak
+                    <input type="radio" name="status_manager[<?= $data['id_data'] ?>]" value="diterima" required>Diterima
+                    <input type="radio" name="status_manager[<?= $data['id_data'] ?>]" value="ditolak" required>Ditolak
                   <?php else : ?>
                     <?php echo $data["status_manager"]; ?>
                   <?php endif; ?>
                 </td>
-              </tr>
-            <?php $no++;
-            } ?>
-          <?php } ?>
-        </table></br>
+                <?php else : ?>
+                    <td>
+                        <!-- Jika posisi adalah "manager", disable input radio -->
+                        <input type="radio" name="status_manager[<?= $data['id_data'] ?>]" value="Diterima" disabled>Diterima
+                        <input type="radio" name="status_manager[<?= $data['id_data'] ?>]" value="Ditolak" disabled>Ditolak
+                    </td>
+                <?php endif; ?>
+            </tr>
+        <?php $no++;
+        } ?>
+    </table>
+<?php ?>
+</br>
         </table>
+
         <input type="submit" name="submit" id="submit" class="btn btn-info" value="Submit" />
     </div>
     </form>
@@ -155,12 +173,14 @@ if ($data['jumlah'] > 0) {
         $('input[type="checkbox"]').click(function() {
           var stt = $(this).val();
           console.log(stt)
+
           function setHide() {
             setTimeout(function() {
               $("#result").removeClass("d-block")
               $("#result").addClass("d-none")
             }, 2000)
           }
+
           $.ajax({
             url: "proses_manager.php",
             method: POST,
@@ -175,20 +195,30 @@ if ($data['jumlah'] > 0) {
             }
           })
         })
+
       })
     </script>
+
     </td>
     </tr>
+
     </tr>
+
     </p>
     </div>
     </div>
   </section>
   <script>
-   document.getElementById("button-toggle").addEventListener("click", () => {
-   document.getElementById("sidebar").classList.toggle("active-sidebar");
-   document.getElementById("main-content").classList.toggle("active-main-content");
+    // event will be executed when the toggle-button is clicked
+    document.getElementById("button-toggle").addEventListener("click", () => {
+
+      // when the button-toggle is clicked, it will add/remove the active-sidebar class
+      document.getElementById("sidebar").classList.toggle("active-sidebar");
+
+      // when the button-toggle is clicked, it will add/remove the active-main-content class
+      document.getElementById("main-content").classList.toggle("active-main-content");
     });
-</script>
+  </script>
 </body>
-</html>
+
+}</html>
