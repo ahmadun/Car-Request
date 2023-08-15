6fda8bc33dc17849ea89ee26843af0df0d9456d5
aini <122358601+Ainizoro@users.noreply.github.com>
2023-08-15 07:40:31 +0700

    Update Add_jadwal.php

--- Add_jadwal.php
+++ Add_jadwal.php
@@ -1,19 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Car Request</title>
-</head>
-<body style="padding:15px;background:#87CEFA">
-	<h4>Tambah Jadwal Keberangkatan Bus</h4>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>USER</title>
+  <!-- bootstrap 5 css -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="css/fontawesome.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
+
+<body style="padding:15px;background: white;">   
+<body>
+    <div class="container">
+      <div class="row">
+            <div class="col-md-4 offset-md-4 mt-5">
+<body style="padding:15px; background:#87CEFA">
 <?php 
-session_start(); // session untuk notifikasi pesan
 echo @$_SESSION['pesan'];
-session_destroy();
 ?>
-<form action="tambah_jadwal.php" method="post">
-<input style="margin-bottom:10px;" type="text" name="plan1" placeholder="Jam Plan 1"><br>
-<input style="margin-bottom:10px;" type="text" name="plan3" placeholder="Jam Plan 3"><br>
-<button type="submit" class="btn btn-info">Simpan</button>
+<body>
+               <div class="card" style="width: auto; height: auto;">
+                <div class="card-title text-center">  
+    <h3 style="font-size: 30px;">Tambah Jadwal Keberangkatan Bus</h3>
+    <div class="card-body">
+    <form method="post" action="tambah_jadwal.php">
+ <table>
+          <form class="form-group" method="post">   
+           <input style="margin-bottom:10px; width: 300px; height: 30px;" type="text" name="plan1" placeholder="Jam Plan 1"><br>   
+            <input style="margin-bottom:10px; width: 300px; height: 30px;" type="text" name="plan3" placeholder="Jam Plan 3"><br>
+                           
+  </select>
+  </div> 
+<div class="card-title text-center">   
+<input type="submit" value="SIMPAN" class="btn btn-info" style="width: 300px;">
+</tr></div>     
+</table>
 </form>
+</div>
+</div>
+</div>
+</div>
+
 </body>
+        </section>
+        </body>
 </html>
