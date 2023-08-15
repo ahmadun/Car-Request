2fe7431712794dae593f714fadd5d23a00d01774
aini <122358601+Ainizoro@users.noreply.github.com>
2023-08-15 09:04:24 +0700

    Update tambah_user.php

--- tambah_user.php
+++ tambah_user.php
@@ -1,8 +1,9 @@
 <?php 
 include 'koneksi.php';
 $username = $_POST['username'];
+$email = $_POST['email'];
 $password = $_POST['password'];
 $hak_akses = $_POST['hak_akses'];
-mysqli_query($koneksi,"insert into login values('','$username','$password','$hak_akses')");
+mysqli_query($koneksi,"insert into login values('','$username', '$email', '$password','$hak_akses')");
 header("location:halaman_admin.php");
 ?>
